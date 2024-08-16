c73c9667d7d423af3314b4325928272a4407297d
saniyasarfaraz <saniyasarfaraz61@gmail.com>
2024-08-16 11:15:21 +0530

    previous changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,95 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Bookdescription from "./components/bookdescription";
+// import Home from "./components/home";
+// import "./App.css";
+// import Books from "./components/assets/books";
+// import BookCard from "./components/BookCard";
+// import Header from "./components/header";
+// import { Routes, Route } from "react-router-dom";
+// import { useState, useEffect } from "react";
+
+// import Sign from "./components/Sign";
+// import Signin from "./components/signin";
+// import Cart from "./components/Cart";
+// import Favourite from "./components/Favourite";
+// import Allbook from "./components/AllBook";
+// import Profile from "./components/Profile";
+// // import { library } from "@fortawesome/fontawesome-svg-core";
+// // import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
+// import Sidebar from "./components/Sidebar";
+
+// // library.add(faShoppingCart, faHeart);
+
+// // import "./components/signin.css";
+
+// // import Footer from "./components/footer";
+
+// function App() {
+//   const [bookList, setBookList] = useState(Books);
+//   function BookList({ bookList }) {
+//     return (
+//       <div className="book-container">
+//         <Header />
+//         {bookList.map((item, index) => (
+//           <BookCard
+//             key={index}
+//             id={index}
+//             img={item.imageSource}
+//             title={item.bookName}
+//             author={item.author}
+//             genre={item.bookGenre}
+//             price={item.price}
+//           />
+//         ))}
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         {/* <Route path="/books" element={<BookList bookList={bookList} />} /> */}
+//         <Route path="/books" element={<Allbook />} />
+//         <Route
+//           path="/Bookdescription/:id"
+//           element={<Bookdescription bookList={bookList} />}
+//         />
+//         <Route path="/sign" element={<Sign />} />
+//         <Route path="/Login" element={<Signin />} />
+//         <Route path="/profile" element={<Profile />}>
+//           <Route index element={<Favourite />} />
+//         </Route>
+//         <Route path="/cart" element={<Cart />} />
+//         <Route path="/favourite" element={<Favourite />}></Route>
+//       </Routes>
+//       <Sidebar />
+//     </div>
+//   );
+// }
+
+// export default App;
 import "bootstrap/dist/css/bootstrap.min.css";
-import Bookdescription from "./components/bookdescription";
+import Bookdescription from "./components/Bookdescription"; // Updated import
 import Home from "./components/home";
 import "./App.css";
 import Books from "./components/assets/books";
 import BookCard from "./components/BookCard";
 import Header from "./components/header";
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import Sign from "./components/Sign";
 import Signin from "./components/signin";
-import Cart from "./components/cart";
+import Cart from "./components/Cart";
 import Favourite from "./components/Favourite";
 import Allbook from "./components/AllBook";
 import Profile from "./components/Profile";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "./components/Sidebar";
-
-// library.add(faShoppingCart, faHeart);
-
-// import "./components/signin.css";
-
-// import Footer from "./components/footer";
 
 function App() {
   const [bookList, setBookList] = useState(Books);
+
   function BookList({ bookList }) {
     return (
       <div className="book-container">
@@ -49,7 +113,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/books" element={<BookList bookList={bookList} />} /> */}
         <Route path="/books" element={<Allbook />} />
         <Route
           path="/Bookdescription/:id"
--- frontend/src/components/bookdescription.js
+++ frontend/src/components/bookdescription.js
@@ -2,28 +2,11 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Books from "./assets/books";
 import Header from "./header";
-import React from "react";
-// import CartIcon from './CartIcon';
-// import FavoritesIcon from './FavoritesIcon';
-import CartIcon from "./header icons/cart.svg";
-import FavoritesIcon from "./header icons/favourite.svg";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Bookdescription = (props) => {
   //to fetch book desc from server
-
-  // const [Data, setData] = useState();
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const response = await axios.get(
-  //       `http://localhost:1000/api/v1/get-book-by-id/${id}`
-  //     );
-  //     setData(response.data.data);
-  //   };
-  //   fetch();
-  // }, []);
-  // console.log("desc " + Data);
-
   const { id } = useParams();
   const navigate = useNavigate();
 
@@ -48,9 +31,15 @@ const Bookdescription = (props) => {
   }, []);
 
   function buy() {
-    alert("Your order placed Suceesfully");
-    navigate("/");
+    if (isLoggedIn) {
+      alert("Your order placed Suceesfully");
+      navigate("/");
+    } else {
+      alert("Please login to buy the book");
+      navigate("/Login");
+    }
   }
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   if (!Data) {
     return <p>Book not found</p>; // Handle case where book is not found
   }
@@ -58,19 +47,6 @@ const Bookdescription = (props) => {
   return (
     <div>
       <Header />
-      <div
-        className="icons-container"
-        style={{ textAlign: "right", margin: "1rem" }}
-      >
-        <FavoritesIcon
-          className="icon"
-          onclick={() => console.log("favorites icon clicked")}
-        />
-        <CartIcon
-          className="icon"
-          onclick={() => console.log("cart icon clicked")}
-        />
-      </div>
       <div
         className="card mb-3 "
         style={{
@@ -116,10 +92,23 @@ const Bookdescription = (props) => {
               </p>
               <button
                 className="btn btn-primary btn-lg"
-                style={{ position: "absolute", bottom: "4rem", width: "6vw" }}
+                style={{ position: "absolute", bottom: "4rem", width: "10vw" }}
                 onClick={buy}
               >
                 Buy Now
+              </button>
+              <button
+                className="btn btn-primary btn-lg"
+                style={{
+                  display: isLoggedIn ? "block" : "none",
+                  position: "absolute",
+                  bottom: "4rem",
+                  right: "2rem",
+                  width: "10vw",
+                }}
+                onClick={buy}
+              >
+                Add to Cart
               </button>
             </div>
           </div>
@@ -130,6 +119,138 @@ const Bookdescription = (props) => {
 };
 
 export default Bookdescription;
+// import { useNavigate, useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import Books from "./assets/books";
+// import Header from "./header";
+// import React from "react";
+// // import CartIcon from './CartIcon';
+// // import FavoritesIcon from './FavoritesIcon';
+// import CartIcon from "./header icons/cart.svg";
+// import FavoritesIcon from "./header icons/favourite.svg";
+// import axios from "axios";
+
+//const Bookdescription = (props) => {
+//to fetch book desc from server
+
+// const [Data, setData] = useState();
+// useEffect(() => {
+//   const fetch = async () => {
+//     const response = await axios.get(
+//       `http://localhost:1000/api/v1/get-book-by-id/${id}`
+//     );
+//     setData(response.data.data);
+//   };
+//   fetch();
+// }, []);
+// console.log("desc " + Data);
+
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+
+//   const [Data, setData] = useState();
+//   useEffect(() => {
+//     const fetch = async () => {
+//       const response = await axios.get(
+//         `http://localhost:1000/api/v1/get-book-by-id/${id}`
+//       );
+//       console.log(response.data.data);
+//       setData(response.data.data);
+//     };
+//     fetch();
+//   }, []);
+//   console.log(Data);
+
+//   // console.log("id is" + id);
+//   // const book = Books[id];
+//   // console.log(book);
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, []);
+
+//   function buy() {
+//     alert("Your order placed Suceesfully");
+//     navigate("/");
+//   }
+//   if (!Data) {
+//     return <p>Book not found</p>; // Handle case where book is not found
+//   }
+
+//   return (
+//     <div>
+//       <Header />
+//       <div
+//         className="icons-container"
+//         style={{ textAlign: "right", margin: "1rem" }}
+//       >
+//         <FavoritesIcon
+//           className="icon"
+//           onclick={() => console.log("favorites icon clicked")}
+//         />
+//         <CartIcon
+//           className="icon"
+//           onclick={() => console.log("cart icon clicked")}
+//         />
+//       </div>
+//       <div
+//         className="card mb-3 "
+//         style={{
+//           minWidth: "80vw",
+//           maxWidth: "90vw",
+//           minHeight: "90vh",
+//           margin: "auto",
+//           marginTop: "12vh",
+//           backgroundColor: "#f5e1bc9e",
+//         }}
+//       >
+//         <div className="row g-0 book-card" style={{}}>
+//           <div className="col-md-6 ">
+//             <img
+//               src={Data.imageSource}
+//               className="img-fluid rounded-start book-img"
+//               alt="a book"
+//               style={{
+//                 margin: "2rem",
+//                 marginLeft: "5vw",
+
+//                 maxHeight: "85vh",
+//               }}
+//             ></img>
+//           </div>
+//           <div className="col-md-6">
+//             <div className="card-body">
+//               <h1 className="card-title">{Data.bookName}</h1>
+//               <p className="card-text text-body-secondary ">
+//                 Author: {Data.author}
+//               </p>
+//               <p className="card-text fs-4" style={{ textAlign: "justify" }}>
+//                 <br></br>
+//                 <strong>Description:</strong> {Data.Bookdescription}
+//               </p>
+//               <p className="card-text fs-5 ">Pages: {Data.pages}</p>
+//               <p className="card-text fs-5 ">Genre: {Data.bookGenre}</p>
+//               <p
+//                 className="card-text  fs-3"
+//                 style={{ position: "absolute", bottom: "10rem" }}
+//               >
+//                 Price: ₹ {Data.price}.00
+//               </p>
+//               <button
+//                 className="btn btn-primary btn-lg"
+//                 style={{ position: "absolute", bottom: "4rem", width: "6vw" }}
+//                 onClick={buy}
+//               >
+//                 Buy Now
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Bookdescription;
 
 // import { useParams } from "react-router-dom";
 // import Books from "./assets/books";
@@ -184,6 +305,91 @@ export default Bookdescription;
 //               <button
 //                 className="btn btn-primary btn-lg"
 //                 style={{ position: "absolute", bottom: "4rem", width: "6vw" }}
+//               >
+//                 Buy Now
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Bookdescription;
+
+// import { useParams } from "react-router-dom";
+// import Books from "./assets/books";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import { WiCloud } from "react-icons/wi";
+
+// const Bookdescription = (props) => {
+//   const { id } = useParams();
+//   const book = Books[id];
+//   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+//   const navigate = useNavigate();
+//   function buy() {
+//     if (isLoggedIn) {
+//       alert("Your order placed Suceesfully");
+//       navigate("/");
+//     } else {
+//       alert("Please login to buy the book");
+//       navigate("/Login");
+//     }
+//   }
+//   return (
+//     <>
+//       <div
+//         className="card mb-3 book-card"
+//         style={{
+//           minWidth: "80vw",
+//           maxWidth: "90vw",
+//           minHeight: "90vh",
+//           margin: "auto",
+//           marginTop: "5vh",
+//         }}
+//       >
+//         <div className="row g-0 ">
+//           <div className="col-md-6">
+//             <img
+//               src={book.imageSource}
+//               className="img-fluid rounded-start book-img"
+//               alt="a book"
+//               style={{
+//                 margin: "2rem",
+//                 marginLeft: "5vw",
+
+//                 maxHeight: "85vh",
+//               }}
+//             ></img>
+
+//             <div style={{ height: "5pxgit" }}>
+//               <WiCloud />
+//             </div>
+//           </div>
+//           <div className="col-md-6">
+//             <div className="card-body">
+//               <h1 className="card-title">{book.bookName}</h1>
+//               <p className="card-text text-body-secondary ">
+//                 Author: {book.author}
+//               </p>
+//               <p className="card-text fs-4">
+//                 <br></br>
+//                 <strong>Description:</strong> {book.Bookdescription}
+//               </p>
+//               <p className="card-text fs-5 ">Pages: {book.pages}</p>
+//               <p className="card-text fs-5 ">Genre: {book.bookGenre}</p>
+//               <p
+//                 className="card-text  fs-3"
+//                 style={{ position: "absolute", bottom: "10rem" }}
+//               >
+//                 Price: ₹ {book.price}.00
+//               </p>
+//               <button
+//                 className="btn btn-primary btn-lg"
+//                 style={{ position: "absolute", bottom: "4rem", width: "6vw" }}
+//                 onClick={buy}
 //               >
 //                 Buy Now
 //               </button>
